8a82e20918f8120dcf8366ab6f2711378447efc0
Alethia Quintero <alethiaq22@gmail.com>
2019-07-20 16:05:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,6 +70,8 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.flatten.each do |holiday, supplies|
+    puts "#{holiday}: #{supplies.to_s}"
 
 end
 
