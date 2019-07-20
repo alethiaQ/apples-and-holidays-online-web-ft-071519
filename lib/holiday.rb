9dc4288736a1780d8f1c0bd6cff13fc49222aa6e
Alethia Quintero <alethiaq22@gmail.com>
2019-07-20 16:27:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.keys. do |season|
+  holiday_hash.keys do |season|
     puts "#{season.capitalize}:"
     season.each do |holiday, supplies|
 
